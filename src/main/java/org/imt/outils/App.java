329fd336403178a009f8366478742794a1bd530b
elkhantachei <ismail.elkhantache@amplexor.com>
2019-05-14 16:33:58 +0200

    Inital commit

---
+++ src/main/java/org/imt/outils/App.java
@@ -0,0 +1,13 @@
+package org.imt.outils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
